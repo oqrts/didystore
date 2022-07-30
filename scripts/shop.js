e300c1756b3428df70150acc2bff2658c8ddcc26
LifeGoal <54373229+SeakMengs@users.noreply.github.com>
2022-07-31 05:27:00 +0700

    Update shop.js

--- scripts/shop.js
+++ scripts/shop.js
@@ -313,7 +313,7 @@ function addItemToCart(cartName, cartPrice, cartImage) {
             <p>${cartName}</p>
             <small>Price: $${cartPrice}</small>
             <br>
-            <a class="cartRemove" href="">Remove</a>
+            <a class="cartRemove">Remove</a>
         </div>
     </div>
     </td>

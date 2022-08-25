303eb09628ba068cb891bb950a18e2dfa288abaa
LifeGoal <54373229+SeakMengs@users.noreply.github.com>
2022-08-25 15:55:37 +0700

    update form

---
+++ scripts/form.js
@@ -0,0 +1,8 @@
+onmousemove = function moveScreen(e) {
+    let clientX = e.clientX;
+    let clientY = e.clientY;
+
+    let screenX = (clientX-880)/880;
+    let screenY = (clientY-370)/370;
+    document.getElementById("screen").style.transform = "translate(" + screenX + "rem," + screenY + "rem)";
+}
